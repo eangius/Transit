28f1b4c3a26414754a9406f2de3cacd82c1ef375
eangius <eliangius@hotmail.com>
2023-12-11 10:06:02 -0500

    implementation

--- source/__init__.py
+++ source/__init__.py
@@ -1,11 +1,15 @@
 #!usr/bin/env python
 
+__version__ = '0.0.0'
 
-PROJ_DIR = '..'
-DATA_DIR = f'{PROJ_DIR}/data'
-IMAGE_DIR = f'{PROJ_DIR}/images'
+import os
 
-# system wide settings.
+# Absolute location of various directories relative to project installation.
+PROJ_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+DATA_DIR = os.path.join(PROJ_DIR, 'data')
+IMAGE_DIR = os.path.join(PROJ_DIR, 'images')
+
+# System wide settings.
 settings = {
     'random_seed': 42,      # for reproducibility
 }
