64ad39a95ae2b5e2ead5c2ad86d316692dc979cd
eangius <eliangius@hotmail.com>
2023-12-14 08:40:42 -0500

    micro progress

--- source/dataset_builder.py
+++ source/dataset_builder.py
@@ -6,18 +6,18 @@
 from typing import *
 from dataclasses import dataclass
 import pandas as pd
+import datetime
 
 
 # Chronologically splits time-series into train & test parts. Ensures
 # testing on newer & non-overlapping training set with optional gap
 # to avoid info leakage.
-def temporal_dataset_splitter(
+def dataset_splitter(
     df: pd.DataFrame,     # data to split
     column: str,          # datetime column
     test_frac: float,     # % of data in test
     gap_frac: float = 0,  # optional % of data to ignore
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
-
     if not (0 < test_frac + gap_frac <= 1):
         raise ValueError("Invalid split fractions.")
 
@@ -37,29 +37,41 @@ def temporal_dataset_splitter(
 
 @dataclass
 class Trip:
-    route: str          # route name & direction
-    df: pd.DataFrame    # sequence of scheduled & deviation of stops
+    name: str         # name name & direction
+    df: pd.DataFrame  # sequence of scheduled & deviation of stops
 
 
-# NOTE: assumes all trips have the same sequence & number of stops.
-# this may not be the case for rush-hour express routes under the same name!
-def temporal_trip_context(
-    df: pd.DataFrame,
+# Groups observations as a collection of daily name trips
+# where each is the sequence of stop status
+# shape = (name, date, trips, stops, 2)
+def spatial_route_context(
+    df: pd.DataFrame,  # raw observations ["Scheduled Time", "Stop Number", "Deviation", "Route"]
+    offset: datetime.timedelta,
 ) -> Iterable[Trip]:
-    return [
-        Trip(route=route, df=df_trips)
+    result = []
+    for route_name, df_routes in df.groupby("Route"):
+        grouper_by_timegap = df_routes.groupby(
+            (df_routes["Scheduled Time"].diff() > offset).cumsum()
+        )
 
-        # split observations by daily routes
-        for (_, route), df_grp in df.groupby([
-            pd.Grouper(key='Scheduled Time', freq='1D', sort=True), "Route"
-        ])
+        df_routes.drop("Route", axis=1, inplace=True)
+        df_routes.sort_values("Scheduled Time", inplace=True)
+        df_routes.set_index("Scheduled Time", inplace=True)
 
-        # split daily routes by repeating trips (via 1st stop sequence)
-        for _, df_trips in \
-            df_grp
-                .drop("Route", axis=1)
-                .set_index("Scheduled Time")
-                .groupby(
-                    df_grp['Stop Number'].eq(df_grp['Stop Number'].iloc[0]).cumsum()
-                )
-    ]
+        # TODO
+        # must still split out staggered busses on the same name
+        # knowing the routes stop ordering.
+
+        result.extend(
+            Trip(route_name, df_trip)
+            for _, df_trip in grouper_by_timegap
+        )
+    return result
+
+
+# Groups observations as a collection of daily name stops
+# where each is the history of trip status for each stop
+def temporal_route_context(
+    df_raw: pd.DataFrame,  # raw observations ["Scheduled Time", "Stop Number", "Deviation", "Route"]
+):
+    raise NotImplementedError
--- source/model_learner.py
+++ source/model_learner.py
@@ -1,5 +1,71 @@
 #!usr/bin/env python
 
 # Internal libraries
+from source.data_loaders import *
+from source.dataset_builder import *
 
-# External libraries
+# Configure pandas to not truncate (...) row & column displays.
+pd.set_option('display.max_rows', 200)
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.width', 1000)
+
+
+"""
+# Download only last 2 years of data to to keep analysis focused on relevant.
+download_transit_history(year="2023")
+download_transit_history(year="2022")
+"""
+
+
+# load the most resent data (need all to preserve name sequence structure)
+df = clean_transit_history(
+    load_transit_history(frac=1.0,  year="2023", month="05")
+)
+
+# decouple all static spatial stop information for later easy lookup.
+# this is equivalent to our vocabulary & embeddings
+# TODO: enrich with more attributes
+df_spatial_stops = df[["Location", "Stop Number"]].set_index("Stop Number")
+df.drop("Location", axis=1, inplace=True)
+
+# TODO:
+# -- should produce a shapefile
+# -- should identify directional routes
+# -- should identify stop order
+# map directional routes to set of stops they contain.
+# this is equivalent to our document/term matrix.
+# df_spatial_routes = df_raw \
+#     .reset_index() \
+#     .drop("Scheduled Time", axis=1)[["Trip", "Stop Number"]] \
+#     .drop_duplicates() \
+#     .value_counts() \
+#     .unstack(fill_value=0)
+# <<dbg should be a sequence of name rather than sparse
+# <<dbg not ideal if routes change path during the day or week
+
+
+# simplify & combine name & destination into directional routes
+df["Route"] = df.apply(lambda row: str(row["Route Number"]) + "__" + str(row["Route Destination"]), axis=1)
+df.drop(["Route Number", "Route Destination"], axis=1, inplace=True)
+
+# prepare remaining historical data for machine learning
+# 1st split chronologically past & future, then split each into individual
+# trips.
+df_old, df_new = dataset_splitter(df, column='Scheduled Time', test_frac=0.20, gap_frac=0.05)
+df_learn = spatial_route_context(df_old, offset=datetime.timedelta(hours=2))
+df_eval = spatial_route_context(df_new, offset=datetime.timedelta(hours=2))
+del df, df_old, df_new
+
+
+# TODO:
+# [0] stationirize & de seasonilize
+# [1] convert schedule/observations into directional-name-trip matrix
+#     -- each cell (stop-id, trip#) cell consists of (scheduled, deviation) feature data.
+#     -- padded to longest name in dataset.
+# [2] spatial model learns from rows of the matrix
+# [3] temporal model learns from columns of the matrix
+# [4] each model will featurize the (stop-id, trip#) the same
+#     ie: expand scheduled into temporal features
+#     ie: expand stop-id into spatial features (lookup)
+#     ie: expand trip#
+
